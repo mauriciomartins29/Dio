ad06ba503692c736e78f094d856576aa36cfcb1e
Maurício Martins <mauricio.martins29@gmail.com>
2021-10-08 21:13:53 -0300

    Series

---
+++ DIO.Series/Enum/Genero.cs
@@ -0,0 +1,17 @@
+namespace Dio.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama =5,
+        Espionagem = 6,
+        Ficcao = 7,
+        Musical = 8,
+        Romance = 9,
+        Suspense = 10,
+        Terror = 11,
+    }
+}
